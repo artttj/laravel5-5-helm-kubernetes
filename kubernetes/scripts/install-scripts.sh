b634cf3a98d4e5b3a2d738f83422d1a6f2edb205
Eamon Keane <eamon.keane1@gmail.com>
2018-03-28 17:52:33 +0100

    updated scripts to specify namespace

--- kubernetes/scripts/install-scripts.sh
+++ kubernetes/scripts/install-scripts.sh
@@ -1,19 +1,21 @@
-helm install stable/mysql --namespace default --name mysql
+namespace=laravel5
 
-kubectl create secret generic --namespace default laravel5-env --from-file=/Users/Eamon/PhpstormProjects/laravel5-5-example/.env
+helm install --wait 400 stable/mysql --namespace ${namespace} --name mysql --set mysqlRootPassword=imApMsfoDt,mysqlDatabase=homestead
 
-kubectl create secret generic --namespace default laravel5-env-seeder --from-file=/Users/Eamon/PhpstormProjects/laravel5-5-example/.env
+kubectl create secret generic --namespace ${namespace} laravel5-env --from-file=/Users/Eamon/PhpstormProjects/laravel5-5-example/.env
+
+kubectl create secret generic --namespace ${namespace} laravel5-env-seeder --from-file=/Users/Eamon/PhpstormProjects/laravel5-5-example/.env
 
 kubectl apply -f /Users/Eamon/PhpstormProjects/laravel5-5-example/kubernetes/kubernetes-yaml/mysql-pvc.yaml
 
 kubectl apply -f /Users/Eamon/PhpstormProjects/laravel5-5-example/kubernetes/kubernetes-yaml/mysql-pv.yaml
 
-helm install stable/mysql --namespace default --name mysql --set persistence.existingClaim="mysql-mysql"
+helm install stable/mysql --namespace ${namespace} --name mysql --set persistence.existingClaim="mysql-mysql"
 
-helm install --namespace default --name laravel5 kubernetes/laravel5
+helm install --namespace ${namespace} --name laravel5 kubernetes/helm/laravel5
 
-helm install --namespace default --name laravel5 kubernetes/laravel5 --set phpfpmImage.tag=orig
+helm install --namespace ${namespace} --name laravel5 kubernetes/helm/laravel5 --set phpfpmImage.tag=orig
 
-kubectl patch deployment --namespace default laravel5-phpfpm --patch '{"spec": {"template": {"spec": {"containers": [{"name": "laravel5-phpfpm","image": "quay.io/eamonkeane/laravel:no-artisan"}]}}}}'
+kubectl patch deployment --namespace ${namespace} laravel5-phpfpm --patch '{"spec": {"template": {"spec": {"containers": [{"name": "laravel5-phpfpm","image": "quay.io/eamonkeane/laravel:no-artisan"}]}}}}'
 
-helm upgrade --install --wait --timeout 400 --namespace default --set phpfpmImage.tag=entrypoint-seeder laravel5 kubernetes/laravel5
+helm upgrade --install --wait --timeout 400 --namespace ${namespace} --set phpfpmImage.tag=entrypoint-seeder laravel5 kubernetes/helm/laravel5
